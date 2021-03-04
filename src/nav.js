cca4deb2e3d9f250e5cf47f9e2f405f614b43580
Patrick Higgins <pat_higgins@hotmail.com>
2021-03-04 13:07:41 -0500

    adds logout button

--- src/nav.js
+++ src/nav.js
@@ -1,18 +1,38 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
 
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function Nav() {
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6">Fresh Approach</Typography>
+        <Typography variant="h6" className={classes.title}>
+          Fresh Approach
+        </Typography>
+        <Button
+          color="inherit"
+          onClick={() => {
+            console.log("SUFF");
+          }}
+        >
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
